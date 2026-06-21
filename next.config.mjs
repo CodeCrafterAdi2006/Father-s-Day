190cf493818285ba101f89712d47f21ebc7660d4
CodeCrafterAdi2006 <eragon4273@gmail.com>
2026-06-21 18:12:46 +0530

    configure app for root domain Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true, // Required for static exports
-  },
-  basePath: "/Father-s-Day", // Required for GitHub Pages subfolder repository routing
-};
+const nextConfig = {};
 
 export default nextConfig;
